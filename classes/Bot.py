7d44b37fb7bc629ecd95b6bfd454d0adeb8bbf7a
Mateo Martinez <mateo.martinez@epitech.eu>
2024-01-01 02:38:39 +0100

    bot class

--- classes/Bot.py
+++ classes/Bot.py
@@ -1,10 +1,68 @@
-import global_var
-import preprocess_img
-from pytesseract import pytesseract
-from OCR import OCRProcessor
+import keyboard
+import pyautogui
+import os
+import random
+import time
+from classes.OCR import OCRClass
+from classes.Logger import LoggerClass
 
 class Bot:
     def __init__(self):
-        ocr_processor  = OCRProcessor()
+        self.Logger = LoggerClass()
+        self.Ocr  = OCRClass()
 
+    def init(self):
+        if not os.path.exists("logs"): os.makedirs("logs")
+        if not os.path.exists("img"): os.makedirs("img")
+        self.Logger.init_logs()
+
+    def run(self):
+        while(True):
+            if keyboard.is_pressed('f8'):
+                print("Stopping script.")
+                break
+
+            #first item in bag
+            pyautogui.moveTo(1278, 206)
+            pyautogui.click()
+            time.sleep(random.uniform(0.5, 1.5))
+            
+            Ocr  = OCRClass()
+            Ocr.screenshoting()
+            Ocr.preprocessing()
+            Ocr.process_name_img()
+            Ocr.process_quantity_img()
+
+            clean_name = Ocr.name.strip().replace('\n', ' ')
+            self.Logger.name_logs(clean_name)
+            self.Logger.quantity_logs(Ocr.quantity)
+
+            match Ocr.quantity:
+                case 1:
+                    Ocr.process_price_1_img()
+                    if (Ocr.price_1 == 1):
+                        pyautogui.write(str(1))
+                        self.Logger.price_1_logs(1)
+                    else:
+                        pyautogui.write(str(Ocr.price_1 - 1))
+                        self.Logger.price_1_logs(Ocr.price_1 - 1)
+                case 10:
+                    Ocr.process_price_10_img()
+                    pyautogui.write(str(Ocr.price_10 - 1))
+                    self.Logger.price_10_logs(Ocr.price_10 - 1)
+                case 100:
+                    Ocr.process_price_100_img()
+                    pyautogui.write(str(Ocr.price_100 - 1))
+                    self.Logger.price_100_logs(Ocr.price_100 - 1)
+
+
+            # sell button
+            pyautogui.moveTo(498, 445)
+            pyautogui.click()
+
+            # popup attention priux trop bas
+            pyautogui.moveTo(863, 689)
+            pyautogui.click()
+            
+            self.Logger.delimiter()
 
--- classes/OCR.py
+++ classes/OCR.py
@@ -6,36 +6,39 @@ from PIL import ImageGrab
 class OCRClass:
     def __init__(self):
             self.custom_config_nbr = r'--psm 10 outputbase digits'
+    
+    def screenshoting(self):
+        self.screenshot(global_var.name_pos, global_var.name_img_name)
+        self.screenshot(global_var.quantity_pos, global_var.quantity_img_name)
+        self.screenshot(global_var.price_1_pos, global_var.price_1_img_name)
+        self.screenshot(global_var.price_10_pos, global_var.price_10_img_name)
+        self.screenshot(global_var.price_100_pos, global_var.price_100_img_name)
 
-            self.screenshot_frame(global_var.name_pos, global_var.name_img_name)
-            self.screenshot_frame(global_var.quantity_pos, global_var.quantity_img_name)
-            self.screenshot_frame(global_var.price_1_pos, global_var.price_1_img_name)
-            self.screenshot_frame(global_var.price_10_pos, global_var.price_10_img_name)
-            self.screenshot_frame(global_var.price_100_pos, global_var.price_100_img_name)
-            self.preprocessed_name_img = preprocess_img.preprocess_image_txt(global_var.name_img_name)
-            self.preprocessed_quantity_img = preprocess_img.preprocess_image_nbr(global_var.quantity_img_name)
-            self.preprocessed_price_1_img = preprocess_img.preprocess_image_nbr(global_var.price_1_img_name)
-            self.preprocessed_price_10_img = preprocess_img.preprocess_image_nbr(global_var.price_10_img_name)
-            self.preprocessed_price_100_img = preprocess_img.preprocess_image_nbr(global_var.price_100_img_name)
+    def preprocessing(self):
+        self.preprocessed_name_img = preprocess_img.preprocess_image_txt(global_var.name_img_name)
+        self.preprocessed_quantity_img = preprocess_img.preprocess_image_nbr(global_var.quantity_img_name)
+        self.preprocessed_price_1_img = preprocess_img.preprocess_image_nbr(global_var.price_1_img_name)
+        self.preprocessed_price_10_img = preprocess_img.preprocess_image_nbr(global_var.price_10_img_name)
+        self.preprocessed_price_100_img = preprocess_img.preprocess_image_nbr(global_var.price_100_img_name)
 
-    def screenshot_frame(self, frame, file_name):
+    def screenshot(self, frame, file_name):
         screenshot = ImageGrab.grab(bbox=(frame['x'], frame['y'], frame['x'] + frame['width'], frame['y'] + frame['height']))
         screenshot.save(file_name)
     
-    def process_name(self):
+    def process_name_img(self):
         self.name = pytesseract.image_to_string(self.preprocessed_name_img)
 
-    def process_quantity(self):
+    def process_quantity_img(self):
         self.quantity = int(pytesseract.image_to_string(self.preprocessed_quantity_img, config=self.custom_config_nbr))
 
     def process_price(self, price, img):
         price = int(pytesseract.image_to_string(img, config=self.custom_config_nbr))
 
-    def process_price_1(self):
+    def process_price_1_img(self):
         self.price_1 = int(pytesseract.image_to_string(self.preprocessed_price_1_img, config=self.custom_config_nbr))
 
-    def process_price_10(self):
+    def process_price_10_img(self):
         self.price_10 = int(pytesseract.image_to_string(self.preprocessed_price_10_img, config=self.custom_config_nbr))
 
-    def process_price_100(self):
+    def process_price_100_img(self):
         self.price_100 = int(pytesseract.image_to_string(self.preprocessed_price_100_img, config=self.custom_config_nbr))
